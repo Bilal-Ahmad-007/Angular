a7859a987f9fc91218281db2c56bac824161669e
Bilal Ahmad <bilalahmad2003170@gmail.com>
2024-07-28 23:04:56 +0500

    template form

--- full-angular-18-bootstrap/src/app/components/form/template-form/template-form.component.ts
+++ full-angular-18-bootstrap/src/app/components/form/template-form/template-form.component.ts
@@ -1,12 +1,49 @@
 import { Component } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
+interface contactDetail{
+  username:string,
+  email:string,
+  password:string,
+  gender:string,
+  termCondition:boolean
+}
 @Component({
   selector: 'app-template-form',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './template-form.component.html',
   styleUrl: './template-form.component.css'
 })
 export class TemplateFormComponent {
+  data:any
+  genderVal:string='';
+detail:contactDetail={
+  username: '',
+  email: '',
+  password: '',
+  gender: '',
+  termCondition: false
+}
 
+genderClick(event:any){
+  this.genderVal=event.target.id
+  this.detail.gender=this.genderVal
+
+}
+formData(){
+this.data=this.detail
+console.log(this.detail)
+
+}
+CleaarformData(){
+  this.detail={
+  username: '',
+  email: '',
+  password: '',
+  gender: '',
+  termCondition: false
+}
+
+console.log(this.detail)
+}
 }
