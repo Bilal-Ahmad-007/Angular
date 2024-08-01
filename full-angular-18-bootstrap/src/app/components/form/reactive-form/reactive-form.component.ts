5add9310af8091e14a19b64df5630ad3a7f29f79
Bilal Ahmad <bilalahmad2003170@gmail.com>
2024-08-01 23:01:20 +0500

    reactive form

--- full-angular-18-bootstrap/src/app/components/form/reactive-form/reactive-form.component.ts
+++ full-angular-18-bootstrap/src/app/components/form/reactive-form/reactive-form.component.ts
@@ -1,12 +1,68 @@
 import { Component } from '@angular/core';
+import { AbstractControl, FormControl, FormGroup, ReactiveFormsModule, ValidationErrors, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-reactive-form',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './reactive-form.component.html',
   styleUrl: './reactive-form.component.css'
 })
 export class ReactiveFormComponent {
+
+
+studentData: FormGroup= new FormGroup({
+  username:new FormControl("",[ Validators.required,Validators.minLength(4),Validators.maxLength(8)]),
+  email:new FormControl("",[Validators.email,Validators.required]),
+  password:new FormControl("",[Validators.required,this.passwordValidator]),
+ 
+  istermConditionConfirm:new FormControl(false,Validators.requiredTrue),
+ 
+})
+  // Custom validator for password
+  passwordValidator(control: AbstractControl): ValidationErrors | null {
+    const value = control.value;
+    if (!value) {
+      return null;
+    }
+
+    const hasUpperCase = /[A-Z]/.test(value);
+    const hasLowerCase = /[a-z]/.test(value);
+    const hasNumeric = /[0-9]/.test(value);
+    const hasSpecialChar = /[!@#$%^&*(),.?":{}|<>]/.test(value);
+    const isValidLength = value.length >= 8;
+
+    const passwordValid = hasUpperCase && hasLowerCase && hasNumeric && hasSpecialChar && isValidLength;
+
+    if (!passwordValid) {
+      return {
+        passwordStrength: {
+          hasUpperCase,
+          hasLowerCase,
+          hasNumeric,
+          hasSpecialChar,
+          isValidLength,
+        }
+      };
+    }
+
+    return null;
+  }
+formVal:any;
+
+
+submit(){
+  this.formVal=this.studentData.value
+  console.log(this.formVal)
+}
+reset(){
+  this.studentData.reset({
+    username:'',
+    email:'',
+    password:'',
+   
+    istermConditionConfirm:false,
+  })
+}
 
 }
